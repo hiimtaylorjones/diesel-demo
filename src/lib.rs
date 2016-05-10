9d713d11b2cfae966e791920e9c3eb1ea8dd3cdd
hiimtaylorjones <taylorjonessoftware@gmail.com>
2016-05-09 22:20:34 -0400

    stopping until new release

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+extern crate diesel;
+extern crate dotenv;
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+#![feature(custom_derive, custom_attribute, plugin)]
+#![plugin(diesel_codegen, dotenv_macros)]
+
+pub mod schema;
+pub mod models;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("Please make sure that you set a valid Database Url.");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connectiong to {}", database_url))
+}
+
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use schema::posts;
+
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    }
+
+    diesel::insert(&new_post).into(posts::table)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
+
 #[cfg(test)]
 mod test {
     #[test]
